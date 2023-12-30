fee47c537426183e3000e751429d764bf1b2e6d8
lvidyadhari <lathavidyadhari97@gmail.com>
2023-12-30 15:34:30 +0530

    shell

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+SOURCE_DIR="/tmp/shellscript-logs"
+
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-SOURCE_DIR=/tmp/shellscript-logs
-
 if [ ! -d $SOURCE_DIR ] # ! denotes opp
 then
     echo -e "$R source diectory: $SOURCE_DIR doesnt exists. $N"
 fi
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +5 -name"*.log")
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +5 -name "*.log")
 
 while IFS= read -r line
 do 
