cd6a60e094c8a985af31a924f0ad4228ff2107dc
lvidyadhari <lathavidyadhari97@gmail.com>
2023-12-30 16:36:52 +0530

    shell

---
+++ 16.ifs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+file=/etc/passwd
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $file ] # ! denotes opp
+then
+    echo -e "$R source diectory: $file doesnt exists. $N"
+fi
+
+while IFS= ":" read -r username password user_id group_id user_fullname home_dir
+do 
+     echo "username: $username"
+     echo "user ID: $user_id"
+     echo "User Full name: $user_fullname"
+done <file
